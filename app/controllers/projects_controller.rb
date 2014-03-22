8125e12817dbc2ed55eb90826c1b92c96da0a771
Karen Sit <karenwsit@gmail.com>
2014-03-22 15:42:16 -0700

    2 pages added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,15 +4,34 @@ class ProjectsController < ApplicationController
 	end
 
 	def create
-	end
+		@project = Project.new(project_params)
 
+    	respond_to do |format|
+	      if @project.save
+	        format.html { redirect_to @project, notice: 'project was successfully created.' }
+	        format.json { render action: 'show', status: :created, location: @project }
+	      else
+	        format.html { render action: 'new' }
+	        format.json { render json: @project.errors, status: :unprocessable_entity }
+	      end
+	    end
+	end	
+  
 	def show
+		@project = Project.find(params[:id])
 	end
 
 	def new
+		@project = Project.new
 	end
 
 	def destroy
 	end
 	
+	private
+
+	def project_params
+	      params.require(:project).permit(:title, :description, :total_fund_ask, :founders, :equity_offered)
+	end
+
 end
